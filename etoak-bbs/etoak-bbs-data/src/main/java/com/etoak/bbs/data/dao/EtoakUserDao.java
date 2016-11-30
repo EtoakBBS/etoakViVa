19f756903c47f07119243ec03f68f61fef89414e
Lin <732798951@qq.com>
2016-11-30 16:43:05 +0800

    第一次整体调整

---
+++ etoak-bbs/etoak-bbs-data/src/main/java/com/etoak/bbs/data/dao/EtoakUserDao.java
@@ -0,0 +1,8 @@
+package com.etoak.bbs.data.dao;
+
+import com.etoak.bbs.data.base.BaseDao;
+import com.etoak.bbs.po.user.EtoakUserPo;
+
+public interface EtoakUserDao extends BaseDao<EtoakUserPo>{
+
+}
